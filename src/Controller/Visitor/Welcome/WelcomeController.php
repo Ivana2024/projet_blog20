81d8a66f688b0bc9ea49dee88be67fa2e65b2b69
ivana <jeremicivana82@gmail.com>
2024-06-19 10:56:18 +0200

    add_module_user

--- src/Controller/Visitor/Welcome/WelcomeController.php
+++ src/Controller/Visitor/Welcome/WelcomeController.php
@@ -6,6 +6,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 class WelcomeController extends AbstractController
 {
+    
     #[Route('/', name: 'visitor_welcome_index', methods:['GET'])]
     public function index(): Response
     {
