8dbd946a49abf213efd9bbaf4337b75b2ee11646
ivana <jeremicivana82@gmail.com>
2024-07-09 21:33:52 +0200

    sprint3-ticket4-feat: Gestion des tags mise en place

--- src/Entity/Tag.php
+++ src/Entity/Tag.php
@@ -2,30 +2,28 @@
 
 namespace App\Entity;
 
-use App\Repository\CategoryRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
+use App\Repository\TagRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Gedmo\Mapping\Annotation as Gedmo;
 
-#[ORM\Entity(repositoryClass: CategoryRepository::class)]
-#[UniqueEntity('name', message:"Cette catégorie existe déjà.")]
-class Category
+#[ORM\Entity(repositoryClass: TagRepository::class)]
+#[UniqueEntity('name', message:"Ce tag existe déjà.")]
+class Tag
 {
-    #[ORM\Id ]
+    #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
-    
-     
+
+
     #[Assert\NotBlank(message: "Le nom est obligatoire.")]
     #[Assert\Length(
         max: 255,
         maxMessage: 'Le nom ne doit pas dépasser {{ limit }} caractères.',
     )]
-    #[ORM\Column(length: 255, unique: true)]
+    #[ORM\Column(length: 255,unique: true)]
     private ?string $name = null;
 
     #[Gedmo\Slug(fields: ['name'])]
@@ -40,16 +38,7 @@ class Category
     #[ORM\Column(nullable: true)]
     private ?\DateTimeImmutable $updatedAt = null;
 
-    /**
-     * @var Collection<int, Post>
-     */
-    #[ORM\OneToMany(targetEntity: Post::class, mappedBy: 'category', orphanRemoval: true)]
-    private Collection $posts;
 
-    public function __construct()
-    {
-        $this->posts = new ArrayCollection();
-    }
 
     public function getId(): ?int
     {
@@ -77,38 +66,11 @@ class Category
     {
         return $this->createdAt;
     }
+
     public function getUpdatedAt(): ?\DateTimeImmutable
     {
         return $this->updatedAt;
     }
 
-    /**
-     * @return Collection<int, Post>
-     */
-    public function getPosts(): Collection
-    {
-        return $this->posts;
-    }
-
-    public function addPost(Post $post): static
-    {
-        if (!$this->posts->contains($post)) {
-            $this->posts->add($post);
-            $post->setCategory($this);
-        }
-
-        return $this;
-    }
-
-    public function removePost(Post $post): static
-    {
-        if ($this->posts->removeElement($post)) {
-            // set the owning side to null (unless already changed)
-            if ($post->getCategory() === $this) {
-                $post->setCategory(null);
-            }
-        }
-
-        return $this;
-    }
+   
 }
